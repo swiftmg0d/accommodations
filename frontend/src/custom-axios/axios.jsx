bde58a8c5202caf59ae497a3b070e37ccf36338e
Zhivche Gligorov <zhivche.gligorov@gmail.com>
2024-06-16 08:18:01 +0200

    add ingress.yml

--- frontend/src/custom-axios/axios.jsx
+++ frontend/src/custom-axios/axios.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const backendUrl =
+  import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api";
+
 export const instance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: backendUrl,
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
